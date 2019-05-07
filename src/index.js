5d70b9947f1d48315f99dc1d28cf4fb510e56d22
Jonathan Ray <jonathanray17@gmail.com>
2019-05-07 11:03:16 -0400

    initials visibility filters in redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ///////////////////// STATE
-import initialState from './base.json';
+import {createStore} from 'redux';
+import initialCards from './base.json';
+
+const VISIBILITY_ALL = 'all';
+const VISIBILITY_CAUGHT = 'caught';
+const VISIBILITY_UNCAUGHT = 'uncaught';
+
+const initialState = {
+    ...initialCards,
+    visibiltyFilter: VISIBILITY_ALL
+};
 // the state is an object with a cards property which is an array of objects
 // { cards: [{}, {}, {}, {}] }
 
@@ -19,22 +29,87 @@ function catchCard(id){
     };
 }
 
-
-
-
+const ACTION_VISIBILITY_ALL = VISIBILITY_ALL;
+const ACTION_VISIBILITY_CAUGHT = VISIBILITY_CAUGHT;
+const ACTION_VISIBILITY_UNCAUGHT = VISIBILITY_UNCAUGHT;
+function setVisibilityAll(){
+    return {
+        type: ACTION_VISIBILITY_ALL
+    };
+}
+function setVisibilityCaught(){
+    return {
+        type: ACTION_VISIBILITY_CAUGHT
+    };
+}
+function setVisibilityUncaught(){
+    return {
+        type: ACTION_VISIBILITY_UNCAUGHT
+    };
+}
 
 // ///////////////////// REDUCER
 function cards(state=initialState, action={type: ''}){
+    switch(action.type){
+        case ACTION_CATCH: 
+            // find the card, set it to "caught"
+            const newState = {
+                ...state,
+                cards: state.cards.map(card => {
+                    if(card.id === action.payload.id){
+                        return {
+                            ...card, 
+                            isCaught: true
+                        };
+                    }
+                    else{
+                        return card;
+                    }
+                })
+            };
+            return newState;
+        break;
+        default:
+            return state;
+        break;
+    }
+}
 
+function visibility(state=initialState, action={type: ''}){
+    switch(action.type){
+        case ACTION_VISIBILITY_ALL :
+            const newState1 = {
+                ...state,
+                visibiltyFilter: VISIBILITY_ALL
+            }
+                return newState1;
+        break;
+        case ACTION_VISIBILITY_CAUGHT :
+            const newState2 = {
+                ...state,
+                visibiltyFilter: VISIBILITY_CAUGHT
+            }
+            return newState2;
+        break;
+        case ACTION_VISIBILITY_UNCAUGHT :
+            const newState3 = {
+                ...state,
+                visibiltyFilter: VISIBILITY_UNCAUGHT
+            }
+            return newState3;
+        break;
+        default:
+            return state;
+        break;
+    }
 }
 
 
 
 
 
-
-
 // ///////////////////// STORE
+const store = createStore(cards);
 
 
 
